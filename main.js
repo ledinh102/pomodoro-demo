ffaf12e12f1bf942e66b792a2843b577a432ac3d
ledinh <lenguyennhutdinh@gmail.com>
2022-11-01 13:05:31 +0700

    merge

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function timeDown() {
 					spanElement[1].innerHTML = 0;
 					spanElement[3].innerHTML = 0;
 					spanElement[4].innerHTML = 0;
-					var audio1 = new Audio("./sound-clock.mp3");
+					var audio1 = new Audio("./sounds/sound-clock.mp3");
 					audio1.play();
 				}
 			}
@@ -31,15 +31,15 @@ function timeDown() {
 
 const app = document.querySelector(".app");
 let interval;
-let audio2 = new Audio("./piano.mp3");
+let audio2 = new Audio("./sounds/piano.mp3");
 app.onclick = function () {
 	audio2.currentTime = 0
 	audio2.play();
 	clearInterval(interval);
 	interval = setInterval(timeDown, 1000);
 	if (this.classList.contains("work")) {
-		this.classList.remove("work");
 		this.classList.add("break");
+		this.classList.remove("work");
 		spanElement[0].innerHTML = 2;
 		spanElement[1].innerHTML = 5;
 	} else {
