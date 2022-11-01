7666ac65a9a5bb80268b11744770792b1b078c7d
ledinh <lenguyennhutdinh@gmail.com>
2022-11-01 10:45:46 +0700

    Master

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const spanElement = document.querySelectorAll("span");
 
 function timeDown() {
-    spanElement[4].innerHTML -= 1;
+	spanElement[4].innerHTML -= 1;
 
 	if (spanElement[4].innerHTML == -1) {
 		spanElement[3].innerHTML -= 1;
@@ -21,29 +21,33 @@ function timeDown() {
 					spanElement[1].innerHTML = 0;
 					spanElement[3].innerHTML = 0;
 					spanElement[4].innerHTML = 0;
-                    var audio = new Audio('./sound-clock');
-                    audio.play();
+					var audio1 = new Audio("./sound-clock.mp3");
+					audio1.play();
 				}
 			}
 		}
 	}
 }
 
-const app = document.querySelector('.app')
-let interval
-app.onclick = function() {
-    interval = setInterval(timeDown, 1000)
-    if(this.classList.contains('work')) {
-        this.classList.remove('work')
-        this.classList.add('break')
-        spanElement[0].innerHTML = 2;
-        spanElement[1].innerHTML = 5;
-    } 
-    else {
-        this.classList.add('work')
-        this.classList.remove('break')
-        spanElement[0].innerHTML = 0;
-        spanElement[1].innerHTML = 5;
-    }
-}
-
+const app = document.querySelector(".app");
+let interval;
+let audio2 = new Audio("./piano.mp3");
+app.onclick = function () {
+	audio2.currentTime = 0
+	audio2.play();
+	clearInterval(interval);
+	interval = setInterval(timeDown, 1000);
+	if (this.classList.contains("work")) {
+		this.classList.remove("work");
+		this.classList.add("break");
+		spanElement[0].innerHTML = 2;
+		spanElement[1].innerHTML = 5;
+	} else {
+		this.classList.add("work");
+		this.classList.remove("break");
+		spanElement[0].innerHTML = 0;
+		spanElement[1].innerHTML = 5;
+	}
+	spanElement[3].innerHTML = 0;
+	spanElement[4].innerHTML = 0;
+};
